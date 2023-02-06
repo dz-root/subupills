73ab689ab01c352ebc3d8f0f17b3df670cef3dcb
dz-root <dz-root@hotmail.fr>
2023-02-06 12:46:02 +0100

    Add Toast, Check username

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,12 +4,37 @@
 	import { SITE_NAME } from '$lib/config'
 	import {web3Init} from '$lib/web3init'
 	import { supabase } from '$lib/supabaseClient';
+    import Toast from '$lib/components//toast.svelte';
 
+    let toastState = {
+        hidden:true,
+        message:'',
+        type:''
+    }
 
     async function uploadImage(e){
         let file = e.target.files[0]
         const {data, error} = await supabase.storage.from('pp').upload($userProfil.address+"/"+$userProfil.name, file)
     }
+
+    async function updateUsername(e){
+        const pattern=/^[a-zA-Z0-9]*$/;
+        const usernameF= $userProfil.name
+        const matches=pattern.exec(usernameF);
+
+        if( matches != null ){
+            const username = await supabase.from('profiles').update({name:$userProfil.name}).eq('address', $userProfil.address)
+            toastState.hidden = false
+            toastState.message = "Username updated successfuly"
+            toastState.type = 'success'
+        }else{
+            toastState.hidden = false
+            toastState.message = "Invalid Username (a-Z 0-9)"
+            toastState.type = 'error'
+        }
+        
+    }
+    
 	onMount(async ()=>{
 		web3Init()
 	})
@@ -26,11 +51,12 @@
 </svelte:head>
 
 <section class="w-11/12 md:w-8/12 mt-16" style="min-height:400px">	
-    <h2>Update Profile</h2>
-	<input type="text" class="w-full bg-transparent border-b text-indigo-600 border-indigo-600" value="{$userProfil.name}">
-    
-    <div class="flex flex-col items-center justify-center w-full">
-        <img src="{$userProfil.image}" alt="" class="rounded-full w-16 h-16">
+
+    <h2 class="text-lg">Update Profile</h2>
+	<input type="text" class="w-full bg-black/30 border-b text-indigo-900 border-indigo-900" bind:value="{$userProfil.name}" on:focusout={(e)=>updateUsername(e)}>
+    <Toast {toastState}/>
+
+    <div class="w-full flex flex-col items-center justify-center mt-4">
         <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-indigo-900 border-dashed rounded-lg cursor-pointer bg-black/20 hover:bg-black/30 transition ">
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
                 <svg aria-hidden="true" class="w-10 h-10 mb-3 text-indigo-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
